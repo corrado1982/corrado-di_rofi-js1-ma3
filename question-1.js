2fa9151759bcb434ea7ea4de92c45a85dd213ded
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-11-03 20:39:57 +0100

    1 done

---
+++ question-1.js
@@ -0,0 +1,11 @@
+
+// function getRemainder(a,b) {
+//     return a % b;
+//     }
+//     const result = getRemainder(6, 4);
+//     console.log(result);
+
+getRemainder = (a, b) => a % b;
+    
+    const result = getRemainder(7, 4);
+    console.log(result);
