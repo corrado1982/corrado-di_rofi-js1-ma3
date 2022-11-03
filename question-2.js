3b0068a43963d0ea46106155e17afad36dfcc34e
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-11-03 23:03:50 +0100

    d

--- question-2.js
+++ question-2.js
@@ -10,27 +10,28 @@ const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const gamesList = document.querySelector(".games-list");
 
 async function getData() {
-    try {
+    
     const response = await fetch(url);
     const data = await response.json();
-
-    console.log(data);
-
-    const item = data.all;
+    const result = data.results;
     
-    console.log(item);
-    gamesList.innerHTML = "we";
-    for (let i = 0; i < data.length; i++) {
+
+    
+    gamesList.innerHTML = "";
+    for (let i = 0; i < result.length; i++) {
+
+        console.log(result[i]);
+
         if (i === 8) {
             break;
         }
-    gamesList.innerHTML += `<div class="div-containers">"ciao"</div>`;
+    gamesList.innerHTML += `<div class="div-containers">${result[i].name}</div>`;
     }
     
     }
-    catch(error) {
-        console.log(error);
-        gamesList.innerHTML = displayError("There is problems calling API");
-    }
-}
+    // catch(error) {
+    //     console.log(error);
+    //     gamesList.innerHTML = displayError("There is problems calling API");
+    
+
 getData();
